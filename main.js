a23db6679b4ed3a0cfd1c320949652720124474a
Colton <kaiserc2@seattleu.edu>
2020-09-03 17:36:41 -0700

    Variable types: string, number, boolean, null, undefined, array, tuple, enum

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ var x = 10;
 var y = 20;
 var sum;
 var title = 'Codevolution';
+var isBeginner = true;
+var total = 0;
+var name = "Colton";
+var sentence = "My name is " + name + "\nI am a beginner in Typescript";
+console.log(sentence);
+//Null and undefined are Subtypes (They can be assigned to any other type of variable (number, string, boolean))
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+//Arrays
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+//Tuple
+//Types can be mixed, but number of elements is fixed. And order of values needs to match order of the types
+var person1 = ['Chris', 22];
+//Enum
+//Values inside are actually numbers, starts at 0 like an array unless specified with '='
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 5] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Green;
+console.log(c);
--- main.ts
+++ main.ts
@@ -7,3 +7,34 @@ const y = 20
 
 let sum
 const title = 'Codevolution'
+
+let isBeginner: boolean = true
+let total: number = 0
+let name: string = "Colton"
+
+let sentence: string = `My name is ${name}
+I am a beginner in Typescript`
+
+console.log(sentence)
+
+//Null and undefined are Subtypes (They can be assigned to any other type of variable (number, string, boolean))
+let n: null = null
+let u: undefined = undefined
+
+let isNew: boolean = null
+let myName: string = undefined
+
+//Arrays
+let list1: number[] = [1,2,3]
+let list2: Array<number> = [1,2,3]
+
+//Tuple
+//Types can be mixed, but number of elements is fixed. And order of values needs to match order of the types
+let person1: [string, number] = ['Chris', 22]
+
+//Enum
+//Values inside are actually numbers, starts at 0 like an array unless specified with '='
+enum Color {Red = 5, Green = 2, Blue}
+
+let c: Color = Color.Green
+console.log(c)
